375214326a5e28dcab236ba08805107679603778
BugHunter-X <askzenai@gmail.com>
2025-02-07 22:20:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+//Incorrect aggregation pipeline
+db.collection.aggregate([
+  {
+    $match: { status: "A" }
+  },
+  {
+    $group: {
+      _id: "$userId",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      userId: "$_id",
+      count: 1
+    }
+  }
+])
+```
